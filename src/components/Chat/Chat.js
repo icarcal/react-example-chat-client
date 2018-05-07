bca71fc81a772ffe78da13cf60bf4f9f77d601c4
Icaro Caldeira <icaro.caldeira@hotmail.com>
2018-05-07 17:57:15 -0300

    add bootstrap and fix layout

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -3,56 +3,56 @@ import ChatList from './ChatList';
 import ioClient from 'socket.io-client';
 import './Chat.css';
 class Chat extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            messages: [
-                'OLA',
-                'ALO',
-                'OK'
-            ],
-            message: '',
-        };
+    this.state = {
+      messages: [
+        'OLA',
+        'ALO',
+        'OK'
+      ],
+      message: '',
+    };
 
-        this.onFormSubmit = this.onFormSubmit.bind(this);
-        this.onInputChange = this.onInputChange.bind(this);
-        this.messageReceived = this.messageReceived.bind(this);
-    }
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+    this.messageReceived = this.messageReceived.bind(this);
+  }
 
-    componentDidMount() {
-        this.io = ioClient('http://localhost:3001/');
-        this.io.on('message:toClient', this.messageReceived);
-    }
+  componentDidMount() {
+    this.io = ioClient('http://localhost:3001/');
+    this.io.on('message:toClient', this.messageReceived);
+  }
 
-    messageReceived(data) {
-        const messages = [...this.state.messages, data];
-        this.setState({ messages: messages });
-    }
+  messageReceived(data) {
+    const messages = [...this.state.messages, data];
+    this.setState({ messages: messages });
+  }
 
-    onFormSubmit(event) {
-        event.preventDefault();
-        this.io.emit('message:toServer', this.state.message);
-        this.setState({ message: '' });
-    }
+  onFormSubmit(event) {
+    event.preventDefault();
+    this.io.emit('message:toServer', this.state.message);
+    this.setState({ message: '' });
+  }
 
-    onInputChange(event) {
-        this.setState({ message: event.target.value });
-    }
+  onInputChange(event) {
+    this.setState({ message: event.target.value });
+  }
 
-    render() {
-        return (
-            <div>
-                <ChatList messages={ this.state.messages }/>
-                <form onSubmit={this.onFormSubmit}>
-                    <input
-                        type="text"
-                        value={ this.state.message }
-                        onChange={ this.onInputChange } />
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <ChatList messages={this.state.messages} />
+        <form onSubmit={this.onFormSubmit}>
+          <input
+            type="text"
+            value={this.state.message}
+            onChange={this.onInputChange} />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Chat;
